46075f99a91f9ca864adb4ab91ce5bb5274b0eb4
Alberto Morgante <alknopfler@gmail.com>
2021-07-07 20:49:16 +0200

    creating the cli flag structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RHsyseng/pullsecret-validator-cli
+
+go 1.16
--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
-import "github.com/RHsyseng/pullsecret-validator-cli/cli"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 
-	f := flag.String("f", "", "Pull secret file to use in json format: -f ./pull-secret.json ")
-	h := flag.Bool("h", false, "Help usage example: ./ocp-release -v nightly -c metal-ipi aws gcp")
+	f := flag.String("f", "", "[Required param] - Pull Secret file in json format. Usage: '-f ./pull-secret.json'")
+	flag.Bool("v", false, "[Optional param] - Array of registry entries from the pull secret file to be validated. Usage: '-c quay.io cloud.openshift.io'")
+	h := flag.Bool("h", false, "Help usage example: './pullsecret-validator-cli -f ./pull-secret.json -c quay.io cloud.openfhit.io' ")
 	flag.Parse()
 	tail := flag.Args()
 
-	if *h {
+	if *h || *f == "" {
+		fmt.Println("This is a tool to validate the Pull Secret file from the command line interface.")
+		fmt.Println("-- Input Required -- The Pull Secret File ")
+		fmt.Println("-- Input Optional -- An array of entries to be only validated as a subset of the whole Pull Secret file.")
+		fmt.Println("-- Output  -- The output will be true when all entries will be validated. If some entries have expired will be shown as an output table")
+		fmt.Println()
+		fmt.Println("Usage: ")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	if len(tail) < 1 {
-		flag.PrintDefaults()
-		os.Exit(2)
-	}
 
-	cli.ValidatePullSecret() //validatePullSecret(*f, tail)
+	fmt.Println(tail, *f)
+	//cli.ValidatePullSecret() //validatePullSecret(*f, tail)
 }
